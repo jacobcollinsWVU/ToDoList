90badb0c2acff1e74d48ca72d28d843cd49ff926
Jacob Collins <jtc00002@mix.wvu.edu>
2023-11-16 17:31:12 -0500

    Formatted HTML and CSS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React, { useState, useRef } from 'react';
 import './App.css';
+
+// Functions to utilize lists
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  const inputRef = useRef(null);
+
+  // Function to add items to the list
+  const addTodo = () => {
+    const text = inputRef.current.value;
+    if (text.trim() !== '') {
+      setTodos((prevTodos) => [...prevTodos, text]);
+      inputRef.current.value = '';
+    }
+  };
+
+  // Function to remove items from the list
+  const removeTodo = (index) => {
+    setTodos((prevTodos) => prevTodos.filter((_, i) => i !== index));
+  };
+
+  // HTML code for the site
+  return (
+    <div className="todo-container">
+      <h1>Todo List</h1>
+      <div className="input-container">
+        <input type="text" ref={inputRef} placeholder="Enter an item" />
+        <button onClick={addTodo}>Add</button>
+      </div>
+      <div className="todo-box">
+        <ul>
+          {todos.map((todo, index) => (
+            <li key={index}>
+              {todo}
+              <button onClick={() => removeTodo(index)}>Remove</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoList />
     </div>
   );
 }
